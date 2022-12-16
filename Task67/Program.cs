ac34430f361e415409ac1908de7276433239009a
Kseniia Des <kseniya250496@mail.ru>
2022-12-16 15:07:44 +0300

    Add Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigits(int num)
+{
+    num = Math.Abs(num);
+    if (num == 0) return 0;
+    return num % 10 + SumOfDigits(num / 10);
+}
+
+int result = SumOfDigits(number);
+Console.WriteLine(result);
+
+
+// int Factorial(int n)
+// {
+// if(n == 1) return 1;
+// else return n * Factorial(n-1);
+// }
